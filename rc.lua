88b1d85b6496429f0803a058640047e620ab9368
Jonas Ornfelt <jonasornfelt@outlook.com>
2026-09-26 12:22:10 +0200

    Taglist: scroll up views the previous tag, down the next (match dwm)

--- rc.lua
+++ rc.lua
@@ -166,8 +166,8 @@ awful.util.taglist_buttons = mytable.join(
     awful.button({ modkey }, 3, function(t)
         if client.focus then client.focus:toggle_tag(t) end
     end),
-    awful.button({ }, 4, function(t) awful.tag.viewnext(t.screen) end),
-    awful.button({ }, 5, function(t) awful.tag.viewprev(t.screen) end)
+    awful.button({ }, 4, function(t) awful.tag.viewprev(t.screen) end),
+    awful.button({ }, 5, function(t) awful.tag.viewnext(t.screen) end)
 )
 
 awful.util.tasklist_buttons = mytable.join(
